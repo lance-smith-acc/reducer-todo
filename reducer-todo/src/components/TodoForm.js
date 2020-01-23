3a0761d6748cd81101c99f7776133fe4a6b53ab7
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2020-01-23 09:36:04 -0600

    Completed MVP

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -5,55 +5,54 @@ import {initialState, reducer} from '../reducers/Reducer'
 export default function TodoForm(){
     
     const [state, dispatch] = useReducer(reducer, initialState);
-    const [newList, setNewList] = useState(initialState)
     const [newItem, setItem] = useState('');
 
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch({type:'ADD', payload:newItem});
-        return setNewList(state);
+        dispatch({type:'ADD', payload:newItem.item});
+      
     }
 
     const handleChanges = e => {
        setItem({
            [e.target.name]:e.target.value,
-           completed:false,
-           id:Date.now()
        });
     }
 
-
-    const handleComplete = () => {
-        dispatch({type:'COMPLETE'});
+    const handleComplete = id => {
+        dispatch({type:'COMPLETE', payload:id});
     }
 
+    const handleClear = () => {
+        dispatch({type:'CLEAR'});
+    }
+
+    console.log(state)
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Todo:</label>
-                <input type="text" name="item" onChange={handleChanges} />
-                <button type='submit'>Add</button>
-            </form>
-
             <div>
-                {
+                <form onSubmit={handleSubmit} onReset={handleClear}>
+                    <label>Todo:</label>
+                    <input type="text" name="item" onChange={handleChanges} />
+                    <button type='submit'>Add</button>
+                    <button type='reset'>Clear</button>
+                </form>
+
+                <div>
+                    {
+                        state.todos !== undefined && state.todos.map((item) => {
+                        return (
+                        <div key={item.id}>
+                            <h1>{item.item}</h1>
+                            <p>Completed: {`${item.completed}`}</p>
+                            <button onClick={() => handleComplete(item.id)}> Mark Complete</button>
+                        </div>
+                        )})
+                    }
+                </div>
                 
-                    newList.map(item => {
-                    return (
-                    <div key={item.id}>
-                        <h1>{item.item}</h1>
-                        <p>Completed: {`${item.completed}`}</p>
-                        <button type="button" onClick={() => handleComplete}> Mark Complete</button>
-                    </div>
-                    )})
-                
-                }
+
             </div>
-            
+        )
+    }
 
-        </div>
-    )
-
-    
-}
 
--- reducer-todo/src/reducers/Reducer.js
+++ reducer-todo/src/reducers/Reducer.js
@@ -1,32 +1,48 @@
-export const initialState = [
-    {
-        item: 'Learn about reducers',
-        completed: false,
-        id: 3892987589
-    },
-    {
-        item: 'Take over the world',
-        completed: false,
-        id: 3892987590
-    },
-    {
-        item: 'Shower',
-        completed: false,
-        id: 3892987591
-    },
+export const initialState = {
+    todos: [
+        {item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589  
+        },
+        {
+            item: 'Take over the world',
+            completed: false,
+            id: 3892987590
+        },
+        {
+            item: 'Shower',
+            completed: false,
+            id: 3892987591
+        },
 
-];
+    ]
+
+}
+
 
 
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD': 
-            return [...state, action.payload];
+            return {
+             ...state, todos:[ ...state.todos, {item:action.payload, completed:false, id:Date.now()}]
+            }
         case 'COMPLETE':
-            return {...state, completed:state.completed.map()};
+            return {
+                ...state, todos:state.todos.map(todo => {
+                    if(todo.id === action.payload){
+                        return {...todo, completed:!todo.completed}
+                    }
+                    else{
+                        return todo;
+                    }
+                })
+            }
         case 'CLEAR':
-            return {}
+            return {
+                ...state, todos:state.todos.filter(item => item.completed !== true)
+            }
         default:
             return state
     }
